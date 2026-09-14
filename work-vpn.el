59c3bf821dcf9d51f9b85bf9799d3ca74e99238b
Yurii <juretz32@yandex.ru>
2026-09-14 21:22:35 +0300

    Simplification

--- work-vpn.el
+++ work-vpn.el
@@ -1,44 +1,23 @@
 (setq work-vpn-connected-p nil)
 
 
-(defun work-vpn-setup ()
-  (with-current-buffer "*work-vpn*"
-    (use-local-map
-     (define-keymap :parent (current-local-map)
-       "g" (lambda ()
-             (interactive)
-             (work-vpn-disconnect)
-             (work-vpn-connect))
-       "k" 'work-vpn-disconnect))
-    (setq-local comint-output-filter-functions
-                (remq 'comint-watch-for-password-prompt
-                      comint-output-filter-functions))))
-
-
 (defun work-vpn-connect ()
   (interactive)
   (let* ((file "~/.password-store/work-vpn.gpg")
          (server (with-temp-buffer
                    (insert-file-contents file)
                    (cadr (split-string (buffer-string) nil t)))))
-    (async-shell-command
+    (start-file-process-shell-command
+     "work-vpn" nil
      (format "gpg -qd %s | head -1 | vpncli -s connect %s && mstsc ~/connect.rdp"
-             file server)
-     "*work-vpn*"))
-  (work-vpn-setup)
-  (message "Connected 🙉")
-  (setq work-vpn-connected-p t))
+             file server))
+    (message "Connected 🙉")
+    (setq work-vpn-connected-p t)))
 
 
 (defun work-vpn-disconnect ()
   (interactive)
-  (when-let* ((b (get-buffer "*work-vpn*"))
-              ((get-buffer-process "*work-vpn*")))
-    (with-current-buffer b
-      (comint-kill-subjob)
-      (sit-for 2)))
-  (async-shell-command "vpncli.exe disconnect" "*work-vpn*")
-  (sit-for 2)
+  (start-file-process-shell-command "work-vpn" nil "vpncli.exe disconnect")
   (message "Disconnected 🙈")
   (setq work-vpn-connected-p nil))
 
